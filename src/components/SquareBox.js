6f502e7d06a1026f240bb8295014c03ad6507b9b
Chris Wilton <chriswilty@zoho.com>
2020-05-01 17:18:27 +0100

    3x3 squares, toggling grid-list view, no animation yet

---
+++ src/components/SquareBox.js
@@ -0,0 +1,52 @@
+import { commonStyle } from 'src/common-style';
+import { template } from 'src/tags/html';
+
+const createTemplate = template`
+  <style>
+    :host {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      overflow: hidden;
+    }
+    
+    ${commonStyle}
+    
+    .wrapper {
+      position: relative;
+      width: 100%;
+      margin: auto;
+      padding-bottom: 100%;
+      background-color: #dfdfdf;
+    }
+    .square {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      flex-wrap: wrap;
+    }
+  </style>
+  <div class="wrapper">
+    <div class="square">
+      <slot></slot>
+    </div>
+  </div>
+`;
+
+class SquareBox extends HTMLElement {
+  static get observedAttributes() {
+    return [ATTR_MODE];
+  }
+
+  constructor() {
+    super();
+
+    this._shadowRoot = this.attachShadow({ mode: 'open' });
+    this._shadowRoot.appendChild(createTemplate());
+  }
+}
+
+window.customElements.define('square-box', SquareBox);
--- src/index.js
+++ src/index.js
@@ -1,55 +1,123 @@
 import { commonStyle } from 'src/common-style';
-import 'src/components/SayHello';
-import * as sayHelloAttributes from 'src/components/SayHello';
 import { template } from 'src/tags/html';
+
+import 'src/components/SquareBox';
+
+const ATTR_MODE = "mode";
+
+// TODO
+// 1. Instead of hard-coding box divs, calculate required number according to number of child elements set.
+// 2. Extract box to separate component.
+const numberOfSquares = numNodes => {
+  let side = 1;
+  while (numNodes > Math.pow(side, 2)) {
+    side += 1;
+  }
+  return Math.pow(side, 2);
+};
 
 const createTemplate = template`
   <style>
+    :host {
+      position: absolute;
+      left: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
     ${commonStyle}
     
-    input {
-      display: block;
-      margin: 4px 0 16px;
-      border: 2px solid #3399ff;
-      padding: 8px 12px;
-      font-size: inherit;
-    }
     .wrapper {
-      padding: 0 8px
+      display: flex;
+      justify-content: center;
+      width: 80vw;
+      height: 90vh;
+    }
+    
+    square-box {
+      max-width: 90vh;
+    }
+    
+    .box {
+      margin: 1px;
+      border: 2px solid forestgreen;
+      border-radius: 8px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background-color: white;
+      font-size: 64px;
+    }
+    .box.grid {
+      width: calc(100% / 3 - 2px);
+      height: calc(100% / 3 - 2px);
+    }
+    .box.list {
+      width: calc(100% - 2px);
+      height: calc(100% / 9 - 2px);
     }
   </style>
 
-  <div>
-    <h1>WebComponents template</h1>
-    <div class="wrapper">
-      <p>In time-honoured fashion ....</p>
-      <label>
-        What's your name, son?
-        <input type="text" />
-      </label>
-      <say-hello></say-hello>
-    </div>
+  <div class="wrapper">
+    <square-box>
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+      <div class="box"></div>
+    </square-box>
   </div>
 `;
 
 class App extends HTMLElement {
+  static get observedAttributes() {
+    return [ ATTR_MODE ];
+  }
+
   constructor() {
     super();
 
     this._shadowRoot = this.attachShadow({ mode: 'open' });
     this._shadowRoot.appendChild(createTemplate());
 
-    this.$input = this._shadowRoot.querySelector('input');
-    const $sayHello = this._shadowRoot.querySelector('say-hello');
-    $sayHello.setAttribute(sayHelloAttributes.ATTR_NAME, "");
+    this.$boxes = this._shadowRoot.querySelectorAll('.box');
 
-    this.$input.addEventListener('input', ({ target: { value } }) => {
-      $sayHello.setAttribute(sayHelloAttributes.ATTR_NAME, value);
-    });
+    // array of elements which each take a "mode" attribute ("grid", "list")
+    this.childElements = new Array(9).fill(0).map((_, i) => i + 1);
   }
 
   connectedCallback() {
-    this.$input.focus();
+    this.mode = 'grid';
+    this.$boxes.forEach((node, index) => {
+      node.innerHTML = `${this.childElements[index]}`;
+      node.addEventListener('click', () => {
+        this.mode = (this.mode === 'grid' ? 'list' : 'grid');
+      });
+    });
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (name === ATTR_MODE && newValue !== oldValue) {
+      console.log(`${ATTR_MODE}=${newValue}`);
+      this.$boxes.forEach(box => {
+        oldValue && box.classList.remove(oldValue);
+        box.classList.add(newValue);
+      });
+    }
+  }
+
+  get mode() {
+    return this.getAttribute(ATTR_MODE);
+  }
+  set mode(value) {
+    this.setAttribute(ATTR_MODE, value);
   }
 }
 
